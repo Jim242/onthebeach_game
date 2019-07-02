cdb833b794acdcc93130aebfc1f352b41ab38af5
JimHiggins <jim242@hotmail.co.uk>
2019-07-03 00:27:29 +0100

    app is responsive to screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Board from "./components/board";
-import initialiseDeck from './deck';
+import initialiseDeck from "./deck";
 
 export default function App() {
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
+  const [dimension, setDimension] = useState(400);
 
   //similar to didMount
   useEffect(() => {
+    resizeBoard();
     setCards(initialiseDeck());
-  //second argument, a value to watch to see if needs to be called again
+    //second argument, a value to watch to see if needs to be called again
   }, []);
 
+  //board will alter size dependant on the screen area
+  useEffect(() => {
+    const resizeListener = window.addEventListener("resize", resizeBoard);
+    return () => window.removeEventListener("resize", resizeListener);
+  });
+
   const handleClick = id => setFlipped([...flipped, id]);
+
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight
+      )
+    );
+  };
 
   return (
     <div>
       <h1>Memory</h1>
       <h2>Can you remember where the cards are?</h2>
-      <Board cards={cards} flipped={flipped} handleClick={handleClick} />
+      <Board
+        dimension={dimension}
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -3,16 +3,18 @@ import PropTypes from "prop-types";
 
 import Card from "../card";
 
-export default function Board({ cards, flipped, handleClick }) {
+import './styles.css';
+
+export default function Board({ dimension, cards, flipped, handleClick }) {
   return (
     <div className="board">
-      {cards.map((card) => (
+      {cards.map(card => (
         <Card
-        key={card.id}
+          key={card.id}
           id={card.id}
           type={card.type}
-          width={100}
-          height={100}
+          width={dimension / 4.5}
+          height={dimension / 4.5}
           flipped={flipped.includes(card.id)} // id array to see if card is flipped
           handleClick={() => handleClick(card.id)}
         />
@@ -21,8 +23,9 @@ export default function Board({ cards, flipped, handleClick }) {
   );
 }
 
-Board.prototype ={
-    cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-    flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
-    handleClick: PropTypes.func.isRequired
-}
+Board.prototype = {
+  dimension: PropTypes.number.isRequired,
+  cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
+  handleClick: PropTypes.func.isRequired
+};
